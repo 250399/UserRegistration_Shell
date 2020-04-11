82d9a1549dca9cd947a3e763d0efd388dec5e239
Kunal <kunaldeshmukh2503@gmail.com>
2020-04-11 01:18:31 -0700

    ADDED:Phonenumber should have country code follow by space and 10 digit number

--- UserReg.sh
+++ UserReg.sh
@@ -1,5 +1,8 @@
-echo Welcome User Registration program
+#!/bin/bash
 
+
+
+echo Welcome User Registration program
 
 read -p"Enter User name :" user
 userFlag=valid
@@ -18,3 +21,15 @@ done
 [ "$userFlag" = "valid" ] && echo Username is Valid || echo Username is Invalid
 read -p"Enter email id :" email
 [[ $email =~ ^[a-zA-Z0-9]+[\.\-\+\_]?[a-zA-Z0-9]*@[a-zA-Z]+[.]?[a-zA-Z]{2,4}[\.]?([a-z]{2})?$ ]] && echo Email is Valid || echo email is Invalid
+read -p"Enter Phone No :" pno
+countryCode=`echo $pno | awk '{ print $1 }'`
+phoneNo=`echo $pno | awk '{ print $2 }'`
+noFlag=valid
+if [[ $countryCode =~ [0-9]{2} ]]
+then
+	[[ $phoneNo =~ [0-9]{10} ]] && : || noFlag=In
+else
+	noFlag=invalid
+fi
+
+[ "$noFlag" = "valid" ] && echo PhoneNumber is vlaid ||echo Invalid Pno
