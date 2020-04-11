a40b961c73284e2229af77dd23bb50ed4b348659
Kunal <kunaldeshmukh2503@gmail.com>
2020-04-11 01:09:34 -0700

    ADDED:Last name starts with Capital and has minimum 3 characters

--- UserReg.sh
+++ UserReg.sh
@@ -1,5 +1,16 @@
-echo Welcome User Registration program
-
-
 read -p"Enter User name :" user
 [[ $user =~ ^[A-Z@][a-z]{2,} ]] && echo Valid || echo Invalid
+userFlag=valid
+for i in 1 2
+do
+	name=`echo $user | awk  '{ print $'$i' }'`
+	echo $name
+	if [[ $name =~ ^[A-Z@][a-z]{2,} ]] 
+	then
+		continue
+	else
+		userFlag=Invalid
+		break
+	fi
+done
+[ "$userFlag" = "valid" ] && echo Valid || echo Invalid
